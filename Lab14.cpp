b244a146f9d41a6759101f0d41683ffc25c399ce
Eirren <edv2019@gmail.com>
2026-02-23 12:08:48 -0800

    Finished Color class, created vector to store Color objects, working on rand from 0 to 256 (borrowed from Lab 4A)

--- Lab14.cpp
+++ Lab14.cpp
@@ -1,13 +1,18 @@
 #include <iostream>
 #include <iomanip>
+#include <vector>
 using namespace std;
 
+// Color vector size
+const int SIZE = 5;
+
+// Color class
 class Color {
-    private:
+    private: // private parts of class containing protected red, green, blue values
         int red;
         int green;
         int blue;
-    public:
+    public: // public getters and setters for red, green, blue privates
         void setRed(int r) {
             red = r;
         }
@@ -26,9 +31,37 @@ class Color {
         int getBlue() {
             return blue;
         }
-
+        // forgot to make the print in previous commit
+        void Print() {
+            cout << setw(3) << "Red value: " << red << " Green value: " << green << " Blue value " << blue << endl;
+        }
 };
 
 int main() {
+
+    // borrowed from Lab 4A
+    srand(time(0));
+    int n;
+    n = rand() % 26 + 25;
+    // vector to store Color objects
+    vector<Color> colorVector;
+    for (int i = 0; i < SIZE; ++i) {
+        Color tmpColor;
+
+    }
+
+    // borrowing from Lab 4A for rand
+    /*
+        int n; // initialize new random int n
+   
+    // cout << n; test cout
+
+    // create and push color structs into vector
+    for (int i = 0; i < n; ++i) {
+        Color tmpColor;
+        // create random values from 0 to 256 for temp struct
+        tmpColor.red = rand() % 255 + 0;
+        */
+
     return 0;
 }
