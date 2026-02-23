35133fca33e8ea5130bb154fd9d5a03c14ded529
Eirren <edv2019@gmail.com>
2026-02-23 12:00:20 -0800

    Created Color class, got sidetracked by job

--- Lab14.cpp
+++ Lab14.cpp
@@ -1,5 +1,33 @@
 #include <iostream>
+#include <iomanip>
 using namespace std;
+
+class Color {
+    private:
+        int red;
+        int green;
+        int blue;
+    public:
+        void setRed(int r) {
+            red = r;
+        }
+        int getRed() {
+            return red;
+        }
+        void setGreen(int g) {
+            green = g;
+        }
+        int getGreen() {
+            return green;
+        }
+        void setBlue(int b) {
+            blue = b;
+        }
+        int getBlue() {
+            return blue;
+        }
+
+};
 
 int main() {
     return 0;
